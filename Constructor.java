de3257161ece00f3f93d24e65ece932419c8b81c
coding-master-for-fun <107452627+coding-master-for-fun@users.noreply.github.com>
2022-06-14 09:16:03 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,11 @@
+package ooppractice;
+class My_name{
+    private int id;
+    private String name;
+    public String getName(){return name;}
+    public void setName(String n){this.name=n;}
+    public int getId(){return id;}
+    public void setId(int i){this.id=i;}
+}
+public class Constructor {
+}
